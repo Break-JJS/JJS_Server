480af009e11f0e8d7964867aa4d551d9dded2c71
submemb <sons6488@gmail.com>
2023-05-03 15:58:46 +0900

    [REFACTOR] User 애노테이션 추가

--- src/main/java/com/cse/jjs/domain/User.java
+++ src/main/java/com/cse/jjs/domain/User.java
@@ -1,18 +1,27 @@
 package com.cse.jjs.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User {
 
     @Id
     private String userID;
+    @Column(nullable = false)
     private String passWord;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private Integer gender;
+    @Column(nullable = false)
     private Double height;
+    @Column(nullable = false)
     private Double weight;
 
-
+    @OneToMany(mappedBy = "user")
+    private List<UserAllergy> userAllergies = new ArrayList<>();
+    @OneToMany(mappedBy = "user")
+    private List<UserDisease> userDiseases = new ArrayList<>();
 }
