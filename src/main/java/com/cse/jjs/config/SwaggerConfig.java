721f04243be7f494ff562e9845778893bdf1d9ae
Son SW <sons69@naver.com>
2023-05-29 13:59:26 +0900

    [ADD] 스웨거 설정파일

---
+++ src/main/java/com/cse/jjs/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package com.cse.jjs.config;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@SuppressWarnings("unckecked")
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    //리소스 핸들러 설
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    // API마다 구분짓기 위한 설정.
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.ant("/**")) // 그중 /api/** 인 URL들만 필터링
+                .build();
+    }
+
+    //swagger 설정.
+    public Docket getDocket(String groupName, Predicate<String> predicate) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(groupName)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cc.kr"))
+                .paths(predicate)
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+    //swagger ui 설정.
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .displayRequestDuration(true)
+                .validatorUrl("")
+                .build();
+    }
+}
